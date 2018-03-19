977d0b20c307232a5342e55dff7b5ff47e0d8f0a
Chia Hua Lu <thegoldflute@users.noreply.github.com>
2018-03-19 17:07:37 -0500

    Saving City Instance midway

--- React/musepy/src/Artists/ArtistInstance.js
+++ React/musepy/src/Artists/ArtistInstance.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from '../HeaderAndFooter/Navigation';
 import Footer from '../HeaderAndFooter/Footer';
 import '../assets/css/artist_instance.css';
+import Black from '../assets/images/black_background.jpg';
 
 
 
@@ -11,29 +12,31 @@ class ArtistInstance extends Component {
 	constructor() {
 		super();
 		this.state= {
+			songFound: false,
+			doneLoading: false,
 			artist: {
 			    "name": "Travis Scott",
 			    "age": 25,
     			doneLoading: false,
-			    "albums": [
+			    albums: [
 			        "Huncho Jack, Jack Huncho",
 			        "Birds in the Trap Sing McKnight",
 			        "Rodeo"
 			    ],
-			    "popular_songs": [
+			    popular_songs: [
 			        "goosebumps",
 			        "Butterfly Effect",
 			        "Saint",
 			        "beibs in the trap",
 			        "pick up the phone"
 			    ],
-			    "birthplace": "Houston, TX",
-			    "genres": [
+			    birthplace: "Houston, TX",
+			    genres: [
 			        "Hip-Hop"
 			    ],
-			    "gender": "male",
-			    "image_url" : "https://i.scdn.co/image/eb266625dab075341e8c4378a177a27370f91903",
-			    "related_artists": [
+			    gender: "male",
+			    image_url : "https://i.scdn.co/image/eb266625dab075341e8c4378a177a27370f91903",
+			    related_artists: [
 			        "Vic Mensa",
 			        "Mike WILL Made-it",
 			        "A$AP Rocky",
@@ -42,7 +45,7 @@ class ArtistInstance extends Component {
 			        "Desiigner",
 			        "21 Savage"
 			    ],
-			    "bio": "Travis Scott is the stage name of Jacques Webster, a Houston-born hip-hop artist and producer affiliated with Kanye West's GOOD Music and T.I.'s Grand Hustle. Scott has a heavily Auto-Tuned half-sung/half-rapped vocal style, and refers to himself as a singer rather than a rapper. He's produced or co-produced tracks by Kanye West, Rihanna, and Drake, and he's appeared on tracks by Jay-Z, Pusha T, Meek Mill, and numerous others. Within four years of his 2012 mainstream arrival, Scott attained platinum singles as a lead artist and songwriter/producer, as well as a pair of Top Five studio albums, the latter of which went to number one. Scott grew up in a suburb of Houston and began making music as a teenager. He formed a duo called the Graduates with Chris Holloway, and they released an EP in 2009. The following year he formed another duo, the Classmates, with OG Chess. Scott produced the duo's two full-lengths, Buddy Rich and Cruis'n USA, and the duo broke up by the end of 2011. After dropping out of college, Scott moved to Los Angeles and began recording music on his own. He met T.I. and eventually Kanye West. Scott was hired as an in-house producer for GOOD Music, and appeared on the label's Cruel Summer compilation in 2012. Scott's debut, Owl Pharaoh, was originally scheduled to be released as a free mixtape in 2012, but as his profile grew (including a placement in XXL Magazine's Freshman Class of 2013), and due to sample clearance issues, the album wasn't released until May of 2013. Featuring guest appearances by T.I. and 2 Chainz (on the single "Upper Echelon") as well as Toro y Moi and Justin Vernon of Bon Iver, the mixtape eventually garnered a nomination for Best Mixtape at the 2013 BET Hip Hop Awards. The buildup to Scott's first proper studio album involved the Days Before Rodeo mixtape, promoted with the singles "Don't Play" and "Mamacita," and the March 2015 U.S. Rodeo Tour, for which Scott headlined, supported by Young Thug and Metro Boomin. Several of the dates sold out. During the same month, Rihanna's Scott-produced hit single "Bitch Better Have My Money" was released. "3500" and "Antidote" were released ahead of Rodeo, Scott's second full-length, which followed in September on Grand Hustle/Epic and debuted at number three on the Billboard 200. Toward the end of the year, "Antidote" peaked at number 16 on the Hot 100 and became Scott's first platinum single. Chatter regarding a follow-up, along with numerous delays, ensued throughout much of 2016. Meanwhile, Scott extended his commercial presence with featured spots on Wiz Khalifa's "Bake Sale," Rihanna's "Woo," and Kanye West's "FML," as well as a collaboration with Young Thug and Quavo, "Pick Up the Phone," a mid-year hit issued as the lead single of Young Thug's JEFFERY. Three months later, after "Bitch Better Have My Money" earned a platinum certification, Scott's second proper album arrived. Titled Birds in the Trap Sing McKnight, after one of Quavo's lines in "Pick Up the Phone," the woozy, mostly midtempo set featured that hit and contributions from the likes of André 3000, Kid Cudi, and Kendrick Lamar. It went straight to the top of the Billboard 200. Leading up to his third LP, Scott appeared on "Kelly Price" from Migos' Culture and on Drake's "Portland" from the More Life project. The latter track peaked within the Top Ten of the Hot 100, Scott's highest chart placement to date. In 2017 Scott dropped the hazy, Migos-esque single "Butterfly Effect" and Huncho Jack, Jack Huncho, a collaborative project with Quavo."
+			    bio: "Travis Scott is the stage name of Jacques Webster, a Houston-born hip-hop artist and producer affiliated with Kanye West\'s GOOD Music and T.I.\'s Grand Hustle. Scott has a heavily Auto-Tuned half-sung/half-rapped vocal style, and refers to himself as a singer rather than a rapper. He\'s produced or co-produced tracks by Kanye West, Rihanna, and Drake, and he\'s appeared on tracks by Jay-Z, Pusha T, Meek Mill, and numerous others. Within four years of his 2012 mainstream arrival, Scott attained platinum singles as a lead artist and songwriter/producer, as well as a pair of Top Five studio albums, the latter of which went to number one. Scott grew up in a suburb of Houston and began making music as a teenager. He formed a duo called the Graduates with Chris Holloway, and they released an EP in 2009. The following year he formed another duo, the Classmates, with OG Chess. Scott produced the duo\'s two full-lengths, Buddy Rich and Cruis\'n USA, and the duo broke up by the end of 2011. After dropping out of college, Scott moved to Los Angeles and began recording music on his own. He met T.I. and eventually Kanye West. Scott was hired as an in-house producer for GOOD Music, and appeared on the label\'s Cruel Summer compilation in 2012. Scott\'s debut, Owl Pharaoh, was originally scheduled to be released as a free mixtape in 2012, but as his profile grew (including a placement in XXL Magazine\'s Freshman Class of 2013), and due to sample clearance issues, the album wasn\'t released until May of 2013. Featuring guest appearances by T.I. and 2 Chainz (on the single \"Upper Echelon\") as well as Toro y Moi and Justin Vernon of Bon Iver, the mixtape eventually garnered a nomination for Best Mixtape at the 2013 BET Hip Hop Awards. The buildup to Scott\'s first proper studio album involved the Days Before Rodeo mixtape, promoted with the singles \"Don\'t Play\" and \"Mamacita,\" and the March 2015 U.S. Rodeo Tour, for which Scott headlined, supported by Young Thug and Metro Boomin. Several of the dates sold out. During the same month, Rihanna\'s Scott-produced hit single \"Bitch Better Have My Money\" was released. \"3500\" and \"Antidote\" were released ahead of Rodeo, Scott\'s second full-length, which followed in September on Grand Hustle/Epic and debuted at number three on the Billboard 200. Toward the end of the year, \"Antidote\" peaked at number 16 on the Hot 100 and became Scott\'s first platinum single. Chatter regarding a follow-up, along with numerous delays, ensued throughout much of 2016. Meanwhile, Scott extended his commercial presence with featured spots on Wiz Khalifa\'s \"Bake Sale,\" Rihanna\'s \"Woo,\" and Kanye West\'s \"FML,\" as well as a collaboration with Young Thug and Quavo, \"Pick Up the Phone,\" a mid-year hit issued as the lead single of Young Thug\'s JEFFERY. Three months later, after \"Bitch Better Have My Money\" earned a platinum certification, Scott\'s second proper album arrived. Titled Birds in the Trap Sing McKnight, after one of Quavo\'s lines in \"Pick Up the Phone,\" the woozy, mostly midtempo set featured that hit and contributions from the likes of André 3000, Kid Cudi, and Kendrick Lamar. It went straight to the top of the Billboard 200. Leading up to his third LP, Scott appeared on \"Kelly Price\" from Migos\' Culture and on Drake\'s \"Portland\" from the More Life project. The latter track peaked within the Top Ten of the Hot 100, Scott\'s highest chart placement to date. In 2017 Scott dropped the hazy, Migos-esque single \"Butterfly Effect\" and Huncho Jack, Jack Huncho, a collaborative project with Quavo."
 			}
 		}
 	}
@@ -65,19 +68,19 @@ class ArtistInstance extends Component {
 									<img class="first-slide" src="/static/images/Travis_scott.jpg" alt="First slide" />
 									<div class="container">
 										<div class="carousel-caption text-left">
-											<h1>Travis Scott</h1>
+											<h1>{this.state.artist.name}</h1>
 										</div>
 										<div class="carousel-caption text-right">
-											<h3><span>Genres:</span> Hip hop</h3>
+											<h3><span>Genres:</span>{this.state.artist.genres}</h3>
 										</div>
 									</div>
 								</div>
 								<div class="carousel-item">
-									<img class="second-slide" src="/static/images/black_background.jpg" alt="Second slide" />
+									<img class="second-slide" src={Black} alt="Second slide" />
 									<div class="container">
 										<div class="carousel-caption text-left">
 											<span>Bio:</span>
-											<p>Travis Scott is the stage name of Jacques Webster, a Houston-born hip-hop artist and producer affiliated with Kanye West's GOOD Music and T.I.'s Grand Hustle. Scott has a heavily Auto-Tuned half-sung/half-rapped vocal style, and refers to himself as a singer rather than a rapper. He's produced or co-produced tracks by Kanye West, Rihanna, and Drake, and he's appeared on tracks by Jay-Z, Pusha T, Meek Mill, and numerous others. Within four years of his 2012 mainstream arrival, Scott attained platinum singles as a lead artist and songwriter/producer, as well as a pair of Top Five studio albums, the latter of which went to number one. Scott grew up in a suburb of Houston and began making music as a teenager. He formed a duo called the Graduates with Chris Holloway, and they released an EP in 2009. The following year he formed another duo, the Classmates, with OG Chess. Scott produced the duo's two full-lengths, Buddy Rich and Cruis'n USA, and the duo broke up by the end of 2011. After dropping out of college, Scott moved to Los Angeles and began recording music on his own. He met T.I. and eventually Kanye West. Scott was hired as an in-house producer for GOOD Music, and appeared on the label's Cruel Summer compilation in 2012. Scott's debut, Owl Pharaoh, was originally scheduled to be released as a free mixtape in 2012, but as his profile grew (including a placement in XXL Magazine's Freshman Class of 2013), and due to sample clearance issues, the album wasn't released until May of 2013. Featuring guest appearances by T.I. and 2 Chainz (on the single "Upper Echelon") as well as Toro y Moi and Justin Vernon of Bon Iver, the mixtape eventually garnered a nomination for Best Mixtape at the 2013 BET Hip Hop Awards.								 The buildup to Scott's first proper studio album involved the Days Before Rodeo mixtape, promoted with the singles "Don't Play" and "Mamacita," and the March 2015 U.S. Rodeo Tour, for which Scott headlined, supported by Young Thug and Metro Boomin. Several of the dates sold out. During the same month, Rihanna's Scott-produced hit single "Bitch Better Have My Money" was released. "3500" and "Antidote" were released ahead of Rodeo, Scott's second full-length, which followed in September on Grand Hustle/Epic and debuted at number three on the Billboard 200. Toward the end of the year, "Antidote" peaked at number 16 on the Hot 100 and became Scott's first platinum single. Chatter regarding a follow-up, along with numerous delays, ensued throughout much of 2016. Meanwhile, Scott extended his commercial presence with featured spots on Wiz Khalifa's "Bake Sale," Rihanna's "Woo," and Kanye West's "FML," as well as a collaboration with Young Thug and Quavo, "Pick Up the Phone," a mid-year hit issued as the lead single of Young Thug's JEFFERY. Three months later, after "Bitch Better Have My Money" earned a platinum certification, Scott's second proper album arrived. Titled Birds in the Trap Sing McKnight, after one of Quavo's lines in "Pick Up the Phone," the woozy, mostly midtempo set featured that hit and contributions from the likes of André 3000, Kid Cudi, and Kendrick Lamar. It went straight to the top of the Billboard 200. Leading up to his third LP, Scott appeared on "Kelly Price" from Migos' Culture and on Drake's "Portland" from the More Life project. The latter track peaked within the Top Ten of the Hot 100, Scott's highest chart placement to date. In 2017 Scott dropped the hazy, Migos-esque single "Butterfly Effect" and Huncho Jack, Jack Huncho, a collaborative project with Quavo.</p>
+											<p>{this.state.artist.bio}</p>
 										</div>
 									</div>
 								</div>
--- React/musepy/src/Cities/CityInstance.js
+++ React/musepy/src/Cities/CityInstance.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Navigation from '../HeaderAndFooter/Navigation';
 import Footer from '../HeaderAndFooter/Footer';
-
+import '../assets/css/city_instance.css';
+import URL from '../URLSpaceUnderscore';
 
 
 class CityInstance extends Component {
@@ -11,11 +12,123 @@ class CityInstance extends Component {
 			<div className="pageContent">
 				<Navigation activeTab={"cities"}/> 
 
-				<h1>City Instance</h1>
-				<p>fdsaafdsadsffdsadsaffasd</p>
-				
-				<Footer />
+				<main role="main">
 
+					<div align="center">
+						<div className="carousel-item titleImage active">
+							<img className="fourth-slide" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg" alt="Austin City Skyline" />
+							<div className="container">
+								<div className="carousel-caption text-right">
+									<h1><span>{URL.lastUrlItem(0) + ","}</span> {URL.lastUrlItem(1)}</h1>
+								</div>
+							</div>
+						</div>
+					</div>   
+						
+
+
+					
+					<div className="container">
+						<div className="row">
+							<div id="map">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d440897.0680945689!2d-98.03359651875269!3d30.3080552998462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b599a0cc032f%3A0x5d9b464bd469d57a!2sAustin%2C+TX!5e0!3m2!1sen!2sus!4v1518562796210" className="mapIframe" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+							</div>
+
+							<div id="playlist">
+								<p class = "h2">The Sound of <br />	Austin, TX</p>
+								<iframe id="spotify" className="shadow" src="https://embed.spotify.com/?uri=spotify:user:thesoundsofspotify:playlist:3gz7G6lax8nkXZ3vBr3n4i&theme=white" width="450" height="80" frameborder="0" allowtransparency="true"></iframe>
+								
+								<ul className="list-group list-group-flush">
+									<li className="list-group-item"><a href="../songs/song1.html">In My Arms Instead - Randy Rogers Band</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Buy Mystelf A Chance - Randy Rogers Band</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Mo City Don - Z-Ro</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Kiss Me In The Dark - Randy Rogers Band</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Corpus Christi Bay - Robert Earl Keen </a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">My Texas(feat. Pat Green) - Josh Abbot Band</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">She's Like Texas - Josh Abbot Band</a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Sad Sad City - Ghostland Observatory </a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Every Girl - Turnpike Troubadors </a></li>
+									<li className="list-group-item"><a href="../songs/song1.html">Feelin' Good Again - Robert Earl Keen </a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<center>
+						<div id="concerts">
+							<p class = "h2">Upcoming Concerts</p>
+								<ul className="list-group">
+									<li className="list-group-item d-flex justify-content-between align-items-center">
+										<img className="concert-img" src="https://d1marr3m5x4iac.cloudfront.net/images/block250/I0-001/039/777/338-2.jpeg_/kesha-macklemore-38.jpeg"/>
+										<p className="concert-text">
+											<a href="../artists/artist1.html">Kesha & Macklemore</a>
+											<br />	Jun 22, 2018 
+											<br />	 7:00 PM 
+											<br />	 Austin360 Amphitheater<br />	
+											<a href="https:austin.eventful.com/events/kesha-macklemore-/E0-001-109666631-1" target="_blank">
+												<span className="badge badge-primary badge-pill">Buy Tickets</span>
+											</a> 
+										</p>	
+									</li>
+									<li className="list-group-item d-flex justify-content-between align-items-center">
+										<img className="concert-img" src="https://d1marr3m5x4iac.cloudfront.net/images/block250/I0-001/004/328/114-2.png_/st-vincent-14.png"/>
+										<p className="concert-text">
+											<a href="../artists/artist1.html">St. Vincent</a>
+											<br />	Feb 22, 2018 
+											<br />	 8:00 PM 
+											<br />	 Austin City Limits Live @ Moody Theater<br />	
+											<a href="https://austin.eventful.com/events/st-vincent-/E0-001-108421756-5" target="_blank">
+												<span className="badge badge-primary badge-pill">Buy Tickets</span>
+											</a> 
+										</p>
+										
+									</li>
+									<li className="list-group-item d-flex justify-content-between align-items-center">
+										<img className="concert-img" src="https://d1marr3m5x4iac.cloudfront.net/images/block250/I0-001/001/219/398-6.jpeg_/mercyme-98.jpeg"/>
+										<p className="concert-text">
+											<a href="../artists/artist1.html">MercyMe</a>
+											<br />	Feb 25,2018 
+											<br />	 7:00 PM 
+											<br />	 Cedar Park Center<br />	
+											<a href="https://austin.eventful.com/events/mercyme-/E0-001-108401219-1" target="_blank">
+												<span className="badge badge-primary badge-pill">Buy Tickets</span>
+											</a> 
+										</p>
+										
+									</li>
+									<li className="list-group-item d-flex justify-content-between align-items-center">
+										<img className="concert-img" src="https://d1marr3m5x4iac.cloudfront.net/images/block250/I0-001/010/408/857-0.png_/the-avett-brothers-57.png"/>
+										<p className="concert-text">
+											<a href="../artists/artist1.html">The Avett Brothers Plus Asleep at the Wheel in Austin</a>  
+											<br />	Mar 3, 2018 
+											<br />	 8:00 PM 
+											<br />	The Long Center<br />	
+											<a href="https://austin.eventful.com/events/avett-brothers-plus-asleep-wheel-/E0-001-108015350-6" target="_blank">
+												<span className="badge badge-primary badge-pill">Buy Tickets</span>
+											</a> 
+										</p>				
+										
+									</li>
+									<li className="list-group-item d-flex justify-content-between align-items-center">
+										<img className="concert-img" src="https://d1marr3m5x4iac.cloudfront.net/images/block250/I0-001/004/216/435-2.jpeg_/styx-35.jpeg"/>
+										<p className="concert-text">
+											<a href="../artists/artist1.html">Styx</a>
+											<br />	Jul 31, 2018 
+											<br />	 7:00 PM 
+											<br />	 Cedar Park Center<br />	
+											<a href="https://austin.eventful.com/events/styx-/E0-001-104032053-7" target="_blank">
+												<span className="badge badge-primary badge-pill">Buy Tickets</span>
+											</a>
+										</p>
+										 
+									</li>
+								</ul>
+						</div>
+					</center>
+					<br />	
+					<br />	
+
+				</main>	
+				<Footer />
 			</div>
 		);
 	}
