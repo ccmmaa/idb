838c379b5b773e5e4fb2bbdf13655dcd64832a5d
Chia Hua Lu <15913503+ChiaHuaLu@users.noreply.github.com>
2018-03-19 22:10:58 -0500

    Basic City Instance Done

--- React/musepy/src/Cities/CityInstance.js
+++ React/musepy/src/Cities/CityInstance.js
@@ -7,6 +7,14 @@ import URL from '../URLSpaceUnderscore';
 
 class CityInstance extends Component {
 
+	constructor() {
+		super();
+		this.state = {
+			city:URL.toString(URL.lastUrlItem(0)),
+			state: URL.toString(URL.lastUrlItem(1))
+		}
+	}
+
 	render() {
 		return(
 			<div className="pageContent">
@@ -19,23 +27,24 @@ class CityInstance extends Component {
 							<img className="fourth-slide" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Austin_Evening.jpg/1200px-Austin_Evening.jpg" alt="Austin City Skyline" />
 							<div className="container">
 								<div className="carousel-caption text-right">
-									<h1><span>{URL.lastUrlItem(0) + ","}</span> {URL.lastUrlItem(1)}</h1>
+									<h1><span>{this.state.city+ ","}</span> {this.state.state}</h1>
 								</div>
 							</div>
 						</div>
 					</div>   
-						
-
-
-					
+					<div class="container">
+						<hr/>
+					</div>
+										
 					<div className="container">
-						<div className="row">
+						<div className="row mapPart1">
 							<div id="map">
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d440897.0680945689!2d-98.03359651875269!3d30.3080552998462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b599a0cc032f%3A0x5d9b464bd469d57a!2sAustin%2C+TX!5e0!3m2!1sen!2sus!4v1518562796210" className="mapIframe" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+								<iframe src={"https://www.google.com/maps/embed/v1/place?key=AIzaSyBLzbYgNk0kZtGmqrS52qbvW0zZi43WeFw&q=" + this.state.city + "+" + this.state.state} allowfullscreen className="mapIframe" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
 							</div>
 
+
 							<div id="playlist">
-								<p class = "h2">The Sound of <br />	Austin, TX</p>
+								<p class = "h2">The Sound of <br />	{this.state.city}, {this.state.state}</p>
 								<iframe id="spotify" className="shadow" src="https://embed.spotify.com/?uri=spotify:user:thesoundsofspotify:playlist:3gz7G6lax8nkXZ3vBr3n4i&theme=white" width="450" height="80" frameborder="0" allowtransparency="true"></iframe>
 								
 								<ul className="list-group list-group-flush">
@@ -126,6 +135,9 @@ class CityInstance extends Component {
 					</center>
 					<br />	
 					<br />	
+					<div class="container">
+						<hr/>
+					</div>
 
 				</main>	
 				<Footer />
