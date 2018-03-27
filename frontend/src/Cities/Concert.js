d546ed01b4c52f64632b302c6f70248e56ade8ce
Chia Hua Lu <15913503+ChiaHuaLu@users.noreply.github.com>
2018-03-26 20:10:37 -0500

    Concerts Artist links to random city, FIXED

--- frontend/src/Cities/Concert.js
+++ frontend/src/Cities/Concert.js
@@ -54,9 +54,9 @@ class Concert extends Component {
 		let time = this.props.concert.time.substring(this.props.concert.time.indexOf('T') + 1);
 		return(
 			<li className="list-group-item d-flex justify-content-between align-items-center">
-				<img className="concert-img" src={this.state.artistImage}/>
+				<img className="concert-img" src={this.state.artistImage} />
 				<p className="concert-text">
-					<a href={this.props.concert.artist_id}>{this.state.artistName}</a>
+					<a href={"/artists/" + this.props.concert.artist_id}>{this.state.artistName}</a>
 					<br />	{months[month]} {day}, {year}
 					<br />	{time} 
 					<br />	{this.props.concert.venue}<br />	
